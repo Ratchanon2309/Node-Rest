a053851fb765cf2152ca05ca736e68fa320736fa
Ratchanon2309 <euroeat25@gmail.com>
2023-09-26 18:57:07 +0700

    up

--- src/MongoDBCRUD.js
+++ src/MongoDBCRUD.js
@@ -87,7 +87,7 @@ app.delete('/books/:id', async (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
